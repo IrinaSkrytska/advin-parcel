54012a613505f744d4342ca25a2d602c32d6ec1e
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2023-02-16 14:17:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const activateAR = (href, isQuickLook, button) => {
   anchor.setAttribute('href', href);
   anchor.click();
 
-  return Notiflix.Loading.hourglass('AR may need a few seconds to load...');
+  // return Notiflix.Loading.hourglass('AR may need a few seconds to load...');
 
   anchor.addEventListener('message', event => {
     button.dispatchEvent(new CustomEvent('quick-look-button-tapped'));
