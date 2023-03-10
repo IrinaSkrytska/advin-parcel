e2b9a9bc802f878ff2d3a148c06a4bf40258728d
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2023-03-10 12:09:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -227,6 +227,7 @@ $(document).ready(function () {
     }
     anchor.setAttribute('href', href);
     anchor.click();
+    console.log('I`m here and notiflix should be working');
 
     Notiflix.Loading.hourglass('AR may need a few seconds to load...', {
       clickToClose: true,
